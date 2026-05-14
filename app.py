43435c0172caef2d0712675e98eedea69dddf7b5
Max Wong <24912862@student.uwa.edu.au>
2026-05-14 19:28:44 +0800

    instead of name displayed being from create itinerary work, it is now based on username. will need to change itineary form fields to remove name field now

--- app.py
+++ app.py
@@ -77,6 +77,8 @@ class Itinerary(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(120), nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    
+    user = db.relationship('User')   
 
     creator_name = db.Column(db.String(100), nullable=False)
     destination = db.Column(db.String(120), nullable=False)
